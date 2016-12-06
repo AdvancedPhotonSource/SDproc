76803b272911675b46c1647bac676f867277b349
Caschmitz6 <40531451+Caschmitz6@users.noreply.github.com>
2016-12-06 00:28:07 +0000

    Many css fixes/improvements.
    git-svn-id: https://subversion.xray.aps.anl.gov/SDPROC_IXNRS/trunk@65 ac1c74cd-98f4-471f-b914-f51f98c2e48c

--- static/modifyDAT/modifyDAT.js
+++ static/modifyDAT/modifyDAT.js
@@ -5,9 +5,36 @@ $(document).ready( function() {
 })
 
 function averageRange(){
-    alert('In Progress');
+    BootstrapDialog.show({
+        title: 'Average Range',
+        message: function(dialog){
+
+            var $form = $('<form></form>');
+            var $leftIn = $('<input type="text" id="leftIn" class="floatL leftIn" placeholder="X-Point from plot(meV)">')
+            var $rightIn = $('<input type="text" id="rightIn" class="floatL rightIn" placeholder="X-Point from plot(meV)">')
+
+
+            $form.append('<label class="leftIn">Left Inpoint</label>').append($leftIn)
+            $form.append('<br/>').append($leftIn)
+
+            $form.append('<label class="rightIn">Right Inpoint</label>').append($rightIn)
+            $form.append('<br/>').append($rightIn)
+            return $form;
+        },
+        buttons: [{
+            label: 'Average',
+            action: function(dialogItself){
+                alert($('#leftIn').val() + ' ' + $('#rightIn').val())
+            }
+        },{
+            label: 'Close',
+            action: function(dialogItself){
+                dialogItself.close();
+            }
+        }]
+    })
 }
 
 function remBackground(){
-    alert('In Progress');
+
 }
