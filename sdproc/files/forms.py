c0f11f84a5c36ec3579809bebf9f41baac964e2f
moraanto <amorales437w@gmail.com>
2019-05-24 16:20:56 -0500

    form now shows select list

--- sdproc/files/forms.py
+++ sdproc/files/forms.py
@@ -7,5 +7,5 @@ from wtforms.validators import DataRequired, Length
 class FileUploadForm(FlaskForm):
     files = MultipleFileField(label='Choose File(s)', validators=[DataRequired(), FileAllowed(['mda', 'txt', 'dat'])])
     comment_character = StringField(label='Character used to comment', validators=[DataRequired(), Length(min=1,max=1)])
-    file_type = SelectField(label='File Type', choices=[(1,'mda'), (2,'txt')], validators=[DataRequired()])
+    file_type = SelectField(label='File Type', choices=[("a",'mda'), ("b",'txt')], validators=[DataRequired()])
     submit = SubmitField(label='Upload')
