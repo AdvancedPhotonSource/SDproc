a36379d2b382d8ec6710776377e211c9ffe9c05a
Antonio Morales <amorales437w@gmail.com>
2019-05-26 04:29:48 -0500

    made new upload page and had to add error checking for multiple files because flask_wtf does not support it yet

--- sdproc/files/forms.py
+++ sdproc/files/forms.py
@@ -1,9 +1,17 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileAllowed
 from wtforms import SubmitField, MultipleFileField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 
 
 class FileUploadForm(FlaskForm):
-    files = MultipleFileField(label='Choose File(s)', validators=[DataRequired(), FileAllowed(['mda', 'txt', 'dat'])])
+    files = MultipleFileField(label='Choose File(s)', validators=[DataRequired()])
     submit = SubmitField(label='Upload')
+
+    @staticmethod
+    def validate_files(self, files):
+        for file in files.data:
+            print file.filename
+            f_ext = file.filename[-3:]
+            print f_ext
+            if f_ext != 'mda' and f_ext != "dat" and f_ext != "txt":
+                raise ValidationError('One or more of your files does not have an approved extension: mda, txt, dat')
