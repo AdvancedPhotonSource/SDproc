7a3a03e94e7632f5f3c7afecc76d81b81bc725cb
Antonio Morales <amorales437w@gmail.com>
2019-05-26 02:04:57 -0500

    added new functions and form to make the file upload process easier for the user. Also moved tree routes to files.routes file

--- sdproc/files/routes.py
+++ sdproc/files/routes.py
@@ -1,6 +1,11 @@
+import json
+
+from sqlalchemy import desc
+from db.db_model import dataFile, db
 from sdproc.files.forms import FileUploadForm
-from flask import Blueprint, render_template, url_for
-from flask_login import login_required
+from flask import Blueprint, render_template, url_for, request
+from flask_login import login_required, current_user
+from sdproc.files.utils import save_files
 
 
 files = Blueprint('files', __name__)
@@ -11,7 +16,160 @@ files = Blueprint('files', __name__)
 def upload_files():
     form = FileUploadForm()
     if form.validate_on_submit():
-        files = form.files.data
-        for file in files:
-            print file
-    return render_template('new_upload.html',title='New Upload',form=form)
+        files_uploaded = form.files.data
+        save_files(files_uploaded)
+    return render_template('new_upload.html', title='New Upload', form=form)
+
+
+'''The beginning of methods for file structure'''
+@files.route("/jsonData", methods=["GET", "POST"])
+def data():
+    data = []
+    currentUser = current_user.get_id()
+
+    if currentUser == 1:
+        nodes = dataFile.query.all()
+    else:
+        nodes = dataFile.query.filter_by(authed=currentUser)
+
+    for node in nodes:
+        id = node.id
+        title = node.name
+        parent = node.parentID
+        type = node.treeType
+
+        if parent == 0:
+            data.append({ "text" : title, "id" : id, "parent" : "#", "type" : type, "state" : { "opened" : "true", "disabled" : "true" } })
+        else:
+            data.append({"text": title, "id": id, "parent": parent, "type" : type})
+
+    with open('static/someD4.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+    return json.dumps(data)
+
+
+@files.route("/scans_data", methods=["GET","POST"])
+def scans_data():
+    data = []
+    currentUser = current_user.get_id()
+
+    if currentUser == 1:
+        nodes = dataFile.query.all()
+    else:
+        nodes = dataFile.query.filter_by(authed=currentUser)
+
+    for node in nodes:
+        id = node.id
+        title = node.name
+        parent = node.parentID
+        type = node.treeType
+        file_type = node.type
+
+        if file_type == "mda" or file_type == "txt" or file_type == "":
+            if parent == 0:
+                data.append({"text": title, "id": id, "parent": "#", "type": type, "state": {"opened": "true", "disabled": "true"}})
+            elif type == "Folder":
+                data.append({"text": title, "id": id, "parent": parent, "type": type, "state": {"disabled": "true"}})
+            else:
+                data.append({"text": title, "id": id, "parent": parent, "type" : type})
+
+    with open('static/scans_data.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+    return json.dumps(data)
+
+
+@files.route("/createNode", methods=["GET","POST"])
+def create():
+    parent = request.form.get("parent")
+    title = request.form.get("title")
+
+    node = dataFile(name=title, path="", comment="This is a folder.", authed=current_user.get_id(), comChar="", type="", parentID=parent, treeType="Folder")
+
+    db.session.add(node)
+    db.session.commit()
+    return "Done"
+
+
+
+@files.route("/renameN", methods=["GET", "POST"])
+def rename():
+    node = request.form.get("node")
+    newName = request.form.get("newName")
+
+    if "j" in node:
+        node = dataFile.query.order_by(desc('id')).first()
+        type = node.type
+        node.name = newName + "." + type
+    else:
+        currNode = dataFile.query.filter_by(id=node).first()
+        type = currNode.type
+        currNode.name = newName + "." + type
+
+    db.session.commit()
+    return "done"
+
+
+@files.route("/moveNode", methods=["GET", "POST"])
+def move():
+    parent = request.form.get("parent")
+    node = request.form.get("node")
+    print parent;
+    print node;
+
+    currNode = dataFile.query.filter_by(id=node).first()
+
+    currNode.parentID = parent
+    db.session.commit()
+    return "done"
+
+
+@files.route("/deleteNode", methods=["GET", "POST"])
+def delete():
+    node = request.form.get("node")
+
+    currNode = dataFile.query.filter_by(id=node).first()
+
+    if currNode.treeType == "File":
+        db.session.delete(currNode)
+    else:
+        recursive(currNode.id)
+        db.session.delete(currNode)
+
+    db.session.commit()
+    return "done"
+
+
+@files.route("/recursiveNode", methods=["GET", "POST"])
+def recursive(parentID):
+    nodes = dataFile.query.filter_by(parentID=parentID)
+    for node in nodes:
+        if node.treeType == "File":
+            db.session.delete(node)
+        else:
+            recursive(node.id)
+            db.session.delete(node)
+
+    return "done"
+
+
+@files.route("/show_NewComment", methods=["GET", "POST"])
+def newC():
+    id = request.form.get("id")
+    node = dataFile.query.filter_by(id=id).first()
+    nodecomment = node.comment
+    return nodecomment
+
+
+@files.route("/saveNC", methods=["GET", "POST"])
+def sc():
+    id = request.form.get("id")
+    newcomment = request.form.get("comment")
+    node = dataFile.query.filter_by(id=id).first()
+    node.comment = newcomment
+    db.session.commit()
+    return "done"
+
+
+'''The end of methods for file structure'''
---
+++ sdproc/files/utils.py
@@ -0,0 +1,82 @@
+import os
+import secrets
+
+from flask import current_app
+from flask_login import current_user
+from sqlalchemy import and_, desc
+
+from db.db_model import db, dataFile, userFiles
+
+"""
+  definitions used to upload files. All of these definitions are used to handle file uploads.
+"""
+
+
+def save_files(form_files):
+    for file in form_files:
+        random_hex = secrets.token_hex(4)
+        f_name, f_ext = os.path.splitext(file.filename)
+        unique_name = random_hex + "_" + f_name + f_ext
+        file_path = os.path.join(current_app.root_path, 'static/uploaded_files/mda', unique_name)
+        save_data_file(f_name, unique_name, file, f_ext)
+        save_user_file()
+        file.save(file_path)
+
+
+def save_data_file(file_name, unique_name, file, f_ext):
+    name = file_name + f_ext
+    path = unique_name
+    authed = str(current_user.id)
+    comChar = comment_character(f_ext, file)
+    type = file_type(f_ext)
+    parentID = root_folder()
+    data_file = dataFile(name=name, path=path, comment="No comment(s)", authed=authed, comChar=comChar, type=type,
+                         parentID=parentID, treeType="File")
+    db.session.add(data_file)
+    db.session.commit()
+
+
+def save_user_file():
+    data_file = dataFile.query.order_by(desc('id')).first()
+    user_file = userFiles(user_id=current_user.id, file_id=data_file.id)
+    db.session.add(user_file)
+    db.session.commit()
+
+
+def root_folder():
+    authed = str(current_user.id)
+    user_root = dataFile.query.filter(and_(dataFile.authed == authed, dataFile.treeType == "Root")).first()
+    parentID = user_root.id
+    return parentID
+
+
+def comment_character(f_ext, file):
+    """
+    This function returns the character used to comment out lines in a file. MDA files default
+    comment character is '#'. TXT and DAT files do not have default comment characters because
+    they are set by the user who created the file. The function reads the first character of a
+    TXT or DAT file and interprets it as the comment character.
+    :param f_ext: File extension. e.g. .mda, .txt., .dat
+    :param file: The file being uploaded.
+    :return: character used to comment out lines in the file
+    """
+    if f_ext == ".mda":
+        comment_char = "#"
+    else:
+        comment_char = file.read(1)
+    return comment_char
+
+
+def file_type(f_ext):
+    """
+    This function removes the dot (.) from a file extension
+    :param f_ext: the file extension. e.g. .mda, .txt, .dat
+    :return: file type. e.g. mda, txt, dat
+    """
+    if f_ext == ".mda":
+        type = "mda"
+    elif f_ext == ".dat":
+        type = "dat"
+    elif f_ext == ".txt":
+        type = "txt"
+    return type
