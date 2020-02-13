c042847090ec0ac181c03adac0e48d630a31c516
moraanto <amorales437w@gmail.com>
2020-02-13 11:26:44 -0600

    fixed overwrite session issue

--- sdproc/sessions/forms.py
+++ sdproc/sessions/forms.py
@@ -3,6 +3,8 @@ from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from db.db_model import SessionFiles
+from sqlalchemy import and_
+
 
 class SessionForm(FlaskForm):
     session_name = StringField(label='Session Name', validators=[DataRequired(), Length(min=2, max=20)])
@@ -11,6 +13,7 @@ class SessionForm(FlaskForm):
 
     @staticmethod
     def validate_session_name(self, session_name):
-        name = SessionFiles.query.filter_by(name=session_name.data).first()
+        name = SessionFiles.query.filter(and_(SessionFiles.name == session_name.data,
+                                              SessionFiles.user_id == current_user.id)).first()
         if name:
             raise ValidationError('That session name is taken. Please choose a different one.')
